19c1dd84fe426af119e7ad23f11cff83fae098d0
Harri Feng <harrifeng@gmail.com>
2021-04-07 20:09:55 +0800

    2021-04-07-20-09

--- 002_merge_sort.py
+++ 002_merge_sort.py
@@ -1,8 +1,8 @@
-def merge_sort(arr, beg, end):
+def quick_sort(arr, beg, end):
     if beg < end:
         mid = part(arr, beg, end)
-        merge_sort(arr, beg, mid - 1)
-        merge_sort(arr, mid + 1, end)
+        quick_sort(arr, beg, mid - 1)
+        quick_sort(arr, mid + 1, end)
     return
 
 
@@ -21,9 +21,9 @@ def part(arr, beg, end):
 
 
 if __name__ == "__main__":
-    l3 = [4, 1, 6, 2, 98, 3, 9, 8, 23, 5]
+    l3 = [2, 8, 7, 1, 3, 5, 6, 4]
     ret = part(l3, 0, len(l3) - 1)
     print(ret, l3)
 
-    merge_sort(l3, 0, len(l3) - 1)
+    quick_sort(l3, 0, len(l3) - 1)
     print(l3)
