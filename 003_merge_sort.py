8ac740c256e152e65e8339924cdabb9e3ded4af2
Harri Feng <harrifeng@gmail.com>
2021-04-08 19:19:05 +0800

    2021-04-08-19-19

--- 003_merge_sort.py
+++ 003_merge_sort.py
@@ -13,8 +13,6 @@ def merge(arr, start1, start2, end):
     l1 = [i for i in arr[start1:start2]]
     l2 = [j for j in arr[start2 : end + 1]]
 
-    print("before-->", l1, l2, arr[start1 : end + 1])
-
     i = 0
     j = 0
     curr = start1
@@ -36,16 +34,12 @@ def merge(arr, start1, start2, end):
         arr[curr] = l2[j]
         curr += 1
         j += 1
-    print("after--->", l1, l2, arr[start1 : end + 1])
-    print("----------------")
+
     return
 
 
 if __name__ == "__main__":
 
-    # ll = [1, 3, 5, 2, 4, 6, 8]
-    # merge(ll, 0, 3, 6)
-    # print(ll)
     l3 = [2, 8, 7, 1, 3, 5, 6, 4]
     print(l3)
     merge_sort(l3, 0, 7)
